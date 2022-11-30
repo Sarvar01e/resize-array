ef3599b81f401a3f91de115acca514c29e135234
Sarvar01e <sernazarov185@gmail.com>
2022-11-30 22:06:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace resize_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraysize = 3;
+            int[] array = new int[arraysize];
+            Console.WriteLine(array.Length);
+            resizes(ref array);
+            Console.WriteLine(array.Length);
+        }
+
+        static void resizes(ref int[] for_resize) 
+        {
+            Console.WriteLine("Enter new size");
+            int newsize = int.Parse(Console.ReadLine());
+            int[] resizedarray = new int[newsize];
+            for_resize = resizedarray;
+        }
+    }
+}
